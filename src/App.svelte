16b85dc1e7a726b96c80f063625e263f60ed233d
Giovanni <gionneves@gmail.com>
2021-08-16 17:39:15 -0300

    Começo do About e modificado App.Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,24 @@
 </script>
 
 <main>
-  <div class="has-text-centered">
-    <h1 class="is-size-1">Teste</h1>
+  <div class="navbar navbar-bg">
+    <h1 class="title">Teste</h1>
+    <h4>teste2</h4>
   </div>
 </main>
+
+<style lang="scss">
+  .navbar {
+    display: flex;
+    max-height: 14em;
+
+    .title {
+      color: #ddd;
+      margin: 0.5em;
+    }
+  }
+
+  .navbar-bg {
+    background-color: #333;
+  }
+</style>
